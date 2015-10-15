dde520b2f95d5a2614ec70c61e7bf1d0301f170a
Jessy Riordan <jessy.riordan@gmail.com>
2015-10-15 18:55:45 -0400

    clickable tacks and hearts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    $( function() {
+      $('.fa-thumb-tack').click( function(event) {
+        event.preventDefault();
+        $(this).toggleClass("yellowTack");
+      });
+    });
+    $( function() {
+      $('.fa-heart').click( function(event) {
+        event.preventDefault();
+        $(this).toggleClass("redHeart");
+      });
+    });
 });
